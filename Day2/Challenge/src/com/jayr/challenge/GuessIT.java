f00efa534687ec140edd26f7c75bd44d1167ddfd
JosephRidge <josephridge18@gmail.com>
2021-01-06 11:10:54 +0300

    Coding the challenge

---
+++ Day2/Challenge/src/com/jayr/challenge/GuessIT.java
@@ -0,0 +1,26 @@
+package com.jayr.challenge;
+
+public class GuessIT {
+    public static void main(String[] args) {
+
+
+        /*
+        Concept : Generate random number and ask user to guess it
+        ask user to guess it until they receive the number
+        after 5 times the game terminates "Game Over"
+
+        //TODO:
+        Show a welcome message
+        AS user their name and return "Hello NAme of user"
+        Ask if we cpould start the game
+        after receiving  a positive answer generate a random number and a them to guess it
+        if the guess is correct , show cobgradulations message then quit
+        if incorrect ask to try again until he gets it,
+        after the user guess anf its incorrect ask him to guess higher or lower depending on the random number generated
+        if he fails 5 times --> show game over and quit
+        * */
+
+
+
+    }
+}
