75b714bbca3170121d31214e52347bef613ae514
JosephRidge <josephridge18@gmail.com>
2021-01-07 23:40:40 +0300

    logic Build

---
+++ Day3/OOPChallenge/src/com/jayafrique/oopchallenge/Skin.java
@@ -0,0 +1,7 @@
+package com.jayafrique.oopchallenge;
+
+public class Skin extends Organ {
+    public Skin(String name, String medicalCondition, String behavior) {
+        super(name, medicalCondition, behavior);
+    }
+}
