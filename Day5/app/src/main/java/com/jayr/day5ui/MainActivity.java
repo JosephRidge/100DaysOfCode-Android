9f7c485fb13652dbdb7df519d0685c138b2814e7
JosephRidge <josephridge18@gmail.com>
2021-01-10 21:12:46 +0300

    some pracitce on using some ui widgets

--- Day5/app/src/main/java/com/jayr/day5ui/MainActivity.java
+++ Day5/app/src/main/java/com/jayr/day5ui/MainActivity.java
@@ -3,12 +3,44 @@ package com.jayr.day5ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView personalizedGreetings;
+    Button btnGreetings;
+    EditText nameInput;
 
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case  R.id.btnGreet:
+                personalizedGreetings.setText(nameInput.getText().toString());
+                Toast.makeText(this, "Hellow  "+nameInput.getText().toString()+" !! .", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        personalizedGreetings =findViewById(R.id.textHellow);
+        btnGreetings = findViewById(R.id.btnGreet);
+        nameInput = findViewById(R.id.userName);
+        btnGreetings.setOnClickListener(this);
+
+//        nameInput = findViewById(R.id.userName);
+
     }
+
+
+
 }
