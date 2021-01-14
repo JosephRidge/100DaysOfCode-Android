2e20046e55991d7750c9266cf6c002b61f6d0d87
JosephRidge <josephridge18@gmail.com>
2021-01-15 00:25:57 +0300

    UI build

--- EndweekChallenge/app/src/main/java/com/jayr/challenge/MainActivity.java
+++ EndweekChallenge/app/src/main/java/com/jayr/challenge/MainActivity.java
@@ -3,13 +3,28 @@ package com.jayr.challenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String>continents = new ArrayList<>();
+        spinner  = findViewById(R.id.continentSpinner);
+
+        continents.add("Africa");
+        continents.add("Antarctica");
+        continents.add( "Asia");
+        continents.add("Australia");
+        continents.add("Europe");
+        continents.add("North America");
+        continents.add("South America");
+        spinner.
 
     }
 }
