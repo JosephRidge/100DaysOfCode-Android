9da158f8b98a629c2f751eda745be9a6e7e2370e
JosephRidge <josephridge18@gmail.com>
2021-01-15 23:00:07 +0300

    Ui build

--- Day10/app/src/main/java/com/jayr/mabibloteque/MainActivity.java
+++ Day10/app/src/main/java/com/jayr/mabibloteque/MainActivity.java
@@ -2,13 +2,28 @@ package com.jayr.mabibloteque;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button booksButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        booksButton =findViewById(R.id.btnBooks);
+
+        booksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,MyBooks.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
