1ebaba89d5c6de802f135194cc2a087187e0c21c
JosephRidge <josephridge18@gmail.com>
2021-01-21 02:15:30 +0300

    MainActivity logic

--- Day10/app/src/main/java/com/jayr/mabibloteque/UImain/MainActivity.java
+++ Day10/app/src/main/java/com/jayr/mabibloteque/UImain/MainActivity.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.jayr.mabibloteque.About;
+import com.jayr.mabibloteque.BookWishList;
+import com.jayr.mabibloteque.CurrentBookRead;
+import com.jayr.mabibloteque.FavouriteBooks;
 import com.jayr.mabibloteque.R;
 import com.jayr.mabibloteque.Utility.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button booksButton, booksReadBtn;
+    Button booksButton, booksReadBtn, booksCurrentBtn,booksWishList,booksFavourite,aboutApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, AlreadyReadBooks.class);
+                startActivity(i);
+            }
+        });
+        booksCurrentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CurrentBookRead.class);
+                startActivity(i);
+            }
+        });
+        booksWishList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, BookWishList.class);
+                startActivity(i);
+            }
+        });
+        booksFavourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FavouriteBooks.class);
+                startActivity(i);
+            }
+        });
+        aboutApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, About.class);
                 startActivity(i);
             }
         });
