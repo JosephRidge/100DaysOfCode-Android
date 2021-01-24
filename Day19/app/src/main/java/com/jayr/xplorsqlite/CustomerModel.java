e1e2913b5aabd40a947ddcd27e2fad50b0f06c82
JosephRidge <josephridge18@gmail.com>
2021-01-24 23:44:41 +0300

    POJO

---
+++ Day19/app/src/main/java/com/jayr/xplorsqlite/CustomerModel.java
@@ -0,0 +1,61 @@
+package com.jayr.xplorsqlite;
+
+public class CustomerModel {
+    Integer ID;
+    String Name;
+    Integer Age;
+    Boolean isActive;
+
+    public CustomerModel(Integer ID, String name, Integer age, Boolean isActive) {
+        this.ID = ID;
+        Name = name;
+        Age = age;
+        this.isActive = isActive;
+    }
+
+    public CustomerModel() {
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getAge() {
+        return Age;
+    }
+
+
+
+    public void setAge(Integer age) {
+        Age = age;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "ID=" + ID +
+                ", Name='" + Name + '\'' +
+                ", Age=" + Age +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
