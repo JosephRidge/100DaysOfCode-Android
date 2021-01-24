26f2c3df2febebcfb0518995b5b3b256f7f1bba0
JosephRidge <josephridge18@gmail.com>
2021-01-24 23:51:40 +0300

    Main Class Logic

--- Day19/app/src/main/java/com/jayr/xplorsqlite/MainActivity.java
+++ Day19/app/src/main/java/com/jayr/xplorsqlite/MainActivity.java
@@ -3,12 +3,37 @@ package com.jayr.xplorsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+private EditText customerName;
+private EditText customerAge;
+private  Button viewCustomers, addCustomer;
+private CustomerModel customerModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    customerName = findViewById(R.id.customerName);
+    customerAge = findViewById(R.id.customerAge);
+    viewCustomers = findViewById(R.id.viewAllCustomers);
+    addCustomer = findViewById(R.id.AddNewCustomer);
+    customerModel = new CustomerModel();
+
+    viewCustomers.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "View All Customers", Toast.LENGTH_SHORT).show();
+        }
+    });
+    addCustomer.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Add Customer", Toast.LENGTH_SHORT).show();
+        }
+    });
     }
 }
