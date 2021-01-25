424839160d73434b428ccb12dce228e2ab8ccc98
JosephRidge <josephridge18@gmail.com>
2021-01-25 21:08:06 +0300

    DB creation

---
+++ Day19/app/src/main/java/com/jayr/xplorsqlite/DataBaseHelper.java
@@ -0,0 +1,43 @@
+package com.jayr.xplorsqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    /**
+     * Create a helper object to create, open, and/or manage a database.
+     * This method always returns very quickly.  The database is not actually
+     * created or opened until one of {@link #getWritableDatabase} or
+     * {@link #getReadableDatabase} is called.
+     *
+     * @param context to use for locating paths to the the database
+     * @param name    of the database file, or null for an in-memory database
+     * @param factory to use for creating cursor objects, or null for the default
+     * @param version number of the database (starting at 1); if the database is older,
+     *                {@link #onUpgrade} will be used to upgrade the database; if the database is
+     *                newer, {@link #onDowngrade} will be used to downgrade the database
+     */
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, "customer.db", null, 1);
+
+        /*
+        * for this cocnstructore what i have done is.
+        * I have removed the parametere: name,factory and version.
+        * And fed in the values directly in the super() method*/
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
