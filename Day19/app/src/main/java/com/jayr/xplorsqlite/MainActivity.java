0a54edd84b571a985828614dffb7d867ce5801da
JosephRidge <josephridge18@gmail.com>
2021-01-27 21:52:48 +0300

    puulling data and adding it to thev list view adapter

--- Day19/app/src/main/java/com/jayr/xplorsqlite/MainActivity.java
+++ Day19/app/src/main/java/com/jayr/xplorsqlite/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 private EditText customerName;
 private EditText customerAge;
 private  Button viewCustomers, addCustomer;
+private ListView customerList;
 
 private Switch isActive ;
     @Override
@@ -26,6 +29,7 @@ private Switch isActive ;
     viewCustomers = findViewById(R.id.viewAllCustomers);
     addCustomer = findViewById(R.id.AddNewCustomer);
     isActive  = findViewById(R.id.activeCustomerSwitch);
+    customerList = findViewById(R.id.Customers);
 
     viewCustomers.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -34,6 +38,8 @@ private Switch isActive ;
             DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
             List<CustomerModel> everyOne = dataBaseHelper.getEveryOne();
             Toast.makeText(MainActivity.this, everyOne.toString(), Toast.LENGTH_SHORT).show();
+            ArrayAdapter customerArrayAdapter = new ArrayAdapter<CustomerModel>(MainActivity.this,android.R.layout.simple_expandable_list_item_1,everyOne);
+            customerList.setAdapter(customerArrayAdapter);
 
         }
     });
@@ -57,6 +63,10 @@ private Switch isActive ;
             DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
             boolean select =  dataBaseHelper.addOne(customerModel);
             Toast.makeText(MainActivity.this, "Success == "+select, Toast.LENGTH_SHORT).show();
+
+
+
+
         }
     });
     }
