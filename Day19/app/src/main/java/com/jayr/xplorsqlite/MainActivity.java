1f6d4a9e52bf412c37c33977fa3c5a5736e56815
JosephRidge <josephridge18@gmail.com>
2021-01-27 22:02:36 +0300

    puulling data and adding it to thev list view adapter

--- Day19/app/src/main/java/com/jayr/xplorsqlite/MainActivity.java
+++ Day19/app/src/main/java/com/jayr/xplorsqlite/MainActivity.java
@@ -13,33 +13,38 @@ import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-private EditText customerName;
-private EditText customerAge;
-private  Button viewCustomers, addCustomer;
-private ListView customerList;
+    public class MainActivity extends AppCompatActivity {
+    private EditText customerName;
+    private EditText customerAge;
+    private  Button viewCustomers, addCustomer;
+    private ListView customerList;
+    private DataBaseHelper dataBaseHelper;
+    private ArrayAdapter customerArrayAdapter;
+    private Switch isActive ;
 
-private Switch isActive ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    customerName = findViewById(R.id.customerName);
-    customerAge = findViewById(R.id.customerAge);
-    viewCustomers = findViewById(R.id.viewAllCustomers);
-    addCustomer = findViewById(R.id.AddNewCustomer);
-    isActive  = findViewById(R.id.activeCustomerSwitch);
-    customerList = findViewById(R.id.Customers);
 
-    viewCustomers.setOnClickListener(new View.OnClickListener() {
+
+        customerName = findViewById(R.id.customerName);
+        customerAge = findViewById(R.id.customerAge);
+        viewCustomers = findViewById(R.id.viewAllCustomers);
+        addCustomer = findViewById(R.id.AddNewCustomer);
+        isActive  = findViewById(R.id.activeCustomerSwitch);
+        customerList = findViewById(R.id.Customers);
+
+
+        dataBaseHelper = new DataBaseHelper(MainActivity.this);
+        showDataOnList(dataBaseHelper);
+        viewCustomers.setOnClickListener(new View.OnClickListener() {
+
         @Override
         public void onClick(View v) {
             Toast.makeText(MainActivity.this, "View All Customers", Toast.LENGTH_SHORT).show();
-            DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
-            List<CustomerModel> everyOne = dataBaseHelper.getEveryOne();
-            Toast.makeText(MainActivity.this, everyOne.toString(), Toast.LENGTH_SHORT).show();
-            ArrayAdapter customerArrayAdapter = new ArrayAdapter<CustomerModel>(MainActivity.this,android.R.layout.simple_expandable_list_item_1,everyOne);
-            customerList.setAdapter(customerArrayAdapter);
+
+            showDataOnList(dataBaseHelper);
 
         }
     });
@@ -50,10 +55,11 @@ private Switch isActive ;
              CustomerModel customerModel;
             try {
 
-            customerModel = new CustomerModel(-1,customerName.getText().toString(),Integer.parseInt(customerAge.getText().toString()),isActive.isChecked());
-            Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+                customerModel = new CustomerModel(-1,customerName.getText().toString(),Integer.parseInt(customerAge.getText().toString()),isActive.isChecked());
+                Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+                showDataOnList(dataBaseHelper);
 
-        }
+            }
         catch (Exception e){
 
             Toast.makeText(MainActivity.this, "Errrorr Creating Customer", Toast.LENGTH_SHORT).show();
@@ -64,10 +70,12 @@ private Switch isActive ;
             boolean select =  dataBaseHelper.addOne(customerModel);
             Toast.makeText(MainActivity.this, "Success == "+select, Toast.LENGTH_SHORT).show();
 
-
-
-
         }
     });
     }
-}
+
+        private void showDataOnList(DataBaseHelper dataBaseHelper) {
+            customerArrayAdapter = new ArrayAdapter<CustomerModel>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, dataBaseHelper.getEveryOne());
+            customerList.setAdapter(customerArrayAdapter);
+        }
+    }
