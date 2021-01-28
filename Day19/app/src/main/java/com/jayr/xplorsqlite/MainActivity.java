e69be371eae20f511a9f0e8af0fbc0c92c20a7d7
JosephRidge <josephridge18@gmail.com>
2021-01-28 22:58:01 +0300

    Modification on the Add button listener

--- Day19/app/src/main/java/com/jayr/xplorsqlite/MainActivity.java
+++ Day19/app/src/main/java/com/jayr/xplorsqlite/MainActivity.java
@@ -37,47 +37,40 @@ import java.util.List;
 
 
         dataBaseHelper = new DataBaseHelper(MainActivity.this);
-        showDataOnList(dataBaseHelper);
-        viewCustomers.setOnClickListener(new View.OnClickListener() {
+//        showDataOnList(dataBaseHelper);
+      showDataOnList(dataBaseHelper);
 
+        viewCustomers.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Toast.makeText(MainActivity.this, "View All Customers", Toast.LENGTH_SHORT).show();
-
             showDataOnList(dataBaseHelper);
-
         }
     });
+
     addCustomer.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 //            Integer ID, String name, Integer age, Boolean isActive
              CustomerModel customerModel;
             try {
-
                 customerModel = new CustomerModel(-1,customerName.getText().toString(),Integer.parseInt(customerAge.getText().toString()),isActive.isChecked());
                 Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
-
-
-
             }
         catch (Exception e){
-
             Toast.makeText(MainActivity.this, "Errrorr Creating Customer", Toast.LENGTH_SHORT).show();
             customerModel = new CustomerModel(-1,"null",0,false);
-
         }
             DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
             boolean select =  dataBaseHelper.addOne(customerModel);
+            showDataOnList(dataBaseHelper);
             Toast.makeText(MainActivity.this, "Success == "+select, Toast.LENGTH_SHORT).show();
-
         }
     });
-    showDataOnList(dataBaseHelper);
-    }
 
-        private void showDataOnList(DataBaseHelper dataBaseHelper) {
-            customerArrayAdapter = new ArrayAdapter<CustomerModel>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, dataBaseHelper.getEveryOne());
+    }
+        private void showDataOnList(DataBaseHelper dataBaseHelper2) {
+            customerArrayAdapter = new ArrayAdapter<CustomerModel>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, dataBaseHelper2.getEveryOne());
             customerList.setAdapter(customerArrayAdapter);
         }
     }
