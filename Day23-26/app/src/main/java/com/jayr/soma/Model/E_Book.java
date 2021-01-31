f103ab6baa6b18a6c7d80d42d2a167f8d5309f2f
JosephRidge <josephridge18@gmail.com>
2021-01-31 23:28:08 +0300

    Build on the Landing page ( Recycler View implementation )

---
+++ Day23-26/app/src/main/java/com/jayr/soma/Model/E_Book.java
@@ -0,0 +1,10 @@
+package com.jayr.soma.Model;
+
+import android.widget.ImageView;
+
+public class E_Book {
+    private String bookTitle;
+    private String shortBookDesc;
+    private ImageView imageview;
+    private String
+}
