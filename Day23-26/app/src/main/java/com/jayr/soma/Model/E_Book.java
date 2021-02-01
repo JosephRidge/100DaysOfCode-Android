17e2db1959e2da904c3ae0e4607a3eefa6ebfd4b
JosephRidge <josephridge18@gmail.com>
2021-02-01 23:01:17 +0300

    POJO Design

--- Day23-26/app/src/main/java/com/jayr/soma/Model/E_Book.java
+++ Day23-26/app/src/main/java/com/jayr/soma/Model/E_Book.java
@@ -6,5 +6,55 @@ public class E_Book {
     private String bookTitle;
     private String shortBookDesc;
     private ImageView imageview;
-    private String
+    private String Chapters;
+
+
+    public E_Book(String bookTitle, String shortBookDesc, ImageView imageview, String chapters) {
+        this.bookTitle = bookTitle;
+        this.shortBookDesc = shortBookDesc;
+        this.imageview = imageview;
+        Chapters = chapters;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getShortBookDesc() {
+        return shortBookDesc;
+    }
+
+    public void setShortBookDesc(String shortBookDesc) {
+        this.shortBookDesc = shortBookDesc;
+    }
+
+    public ImageView getImageview() {
+        return imageview;
+    }
+
+    public void setImageview(ImageView imageview) {
+        this.imageview = imageview;
+    }
+
+    public String getChapters() {
+        return Chapters;
+    }
+
+    public void setChapters(String chapters) {
+        Chapters = chapters;
+    }
+
+    @Override
+    public String toString() {
+        return "E_Book{" +
+                "bookTitle='" + bookTitle + '\'' +
+                ", shortBookDesc='" + shortBookDesc + '\'' +
+                ", imageview=" + imageview +
+                ", Chapters='" + Chapters + '\'' +
+                '}';
+    }
 }
